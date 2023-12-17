d4396ac1262bcf0468779b89fc0fb263111616f8
JobaerAbdi <jobaerrahman757@gmail.com>
2023-12-17 20:50:06 +0600

    interface and model created

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,39 @@
+
+export type TUserName = {
+    firstName: string;
+    middleName: string;
+    lastName: string
+};
+export type TBloodGroup = "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+export type TGuardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContactNo: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContactNo: string;
+};
+export type TLocalGuardian= {
+    name: string;
+    occupation:string;
+    contactNo: string;
+    address: string;
+};
+
+
+export type TStudent = {
+    id: string;
+    name: TUserName;
+    dateOfBirth?: string;
+    gender: 'male' | 'female';
+    email: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    bloodGroup?: TBloodGroup;
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: TGuardian;
+    localGuardian:TLocalGuardian;
+    profileImage?: string;
+    isActive: 'active'| 'blocked';
+};
---
+++ src/modules/student/student.model.ts
@@ -0,0 +1,118 @@
+import { Schema, model } from 'mongoose';
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  TUserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<TLocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const bloodGroup = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+
+const studentSchema = new Schema<TStudent>({
+  id: {
+    type: String,
+  },
+  name: userNameSchema,
+  dateOfBirth: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    enum: ['male', 'female'],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  emergencyContactNo: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: {
+    type: String,
+    enum: bloodGroup,
+  },
+  presentAddress: {
+    type: String,
+    required: true,
+  },
+  permanentAddress: {
+    type: String,
+    required: true,
+  },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImage: {
+    type: String,
+  },
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+  },
+});
+
+export const Student = model<TStudent>('Student', studentSchema);
