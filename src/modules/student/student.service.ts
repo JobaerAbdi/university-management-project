0e81859d578f21dfabfd33d27deed323c0dd9603
JobaerAbdi <jobaerrahman757@gmail.com>
2023-12-29 12:16:47 +0600

    chining populate method use

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -1,14 +1,21 @@
 import { Student } from './student.model';
 
 const getAllStudentsFromDB = async () => {
-  const result = await Student.find().populate('admissionSemester');
+  const result = await Student.find()
+  .populate('user')
+  .populate('admissionSemester')
+  .populate('academicDepartment');  // this is chining populate methods.
   return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findById(id).populate('admissionSemester');
+  const result = await Student.findById(id)
+  .populate('user')
+  .populate('admissionSemester')
+  .populate('academicDepartment');  // this is chining populate methods.
   return result;
 };
+
 
 export const studentServices = {
   getAllStudentsFromDB,
