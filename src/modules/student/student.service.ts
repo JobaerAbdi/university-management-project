84ea9ce09d124197aa6910aec238d0fb5d7a9c80
JobaerAbdi <jobaerrahman757@gmail.com>
2024-01-03 14:15:57 +0600

    pageination updated

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -17,9 +17,9 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     })),
   });
 
-  const excludeFields = ['searchTerm', 'sort', 'limit', 'page'];
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
   excludeFields.forEach((elem) => delete queryObj[elem]);
-  console.log({query}, {queryObj});
+  // console.log({query}, {queryObj});
   
 
   const filterQuery = searchQuery
@@ -53,8 +53,18 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 
   const paginateQuery = sortQuery.skip(skip)
   
-  const limitQuery = await paginateQuery.limit(limit);
-  return limitQuery;
+  const limitQuery = paginateQuery.limit(limit);
+
+  // field limiting
+  let fields = '-__v'
+  if(query.fields){
+    fields = (query.fields as string).split(',').join(' ')
+    // console.log({fields});
+    
+  }
+
+  const fieldQuery = await limitQuery.select(fields)
+  return fieldQuery;
 };
 
 
