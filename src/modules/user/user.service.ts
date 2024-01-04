8765fb977e9512d2bb725fa8b43072bcb4ac057d
JobaerAbdi <jobaerrahman757@gmail.com>
2024-01-04 19:06:13 +0600

    create faculy with transection and rollback

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,21 +5,24 @@ import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
-import { generateStudentId } from './user.utils';
+import { generateFacultyId, generateStudentId } from './user.utils';
 import { TFaculty } from '../faculty/faculty.interface';
+import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 import { Faculty } from '../faculty/faculty.model';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
-  
   const session = await startSession();
 
   try {
     session.startTransaction();
     const userData: Partial<TUser> = {};
-    const admissionSemester = await AdmissionSemester.findById(payload.admissionSemester);
-    userData.id = await generateStudentId(admissionSemester);
     userData.password = password || (config.default_password as string);
     userData.role = 'student';
+    const admissionSemester = await AdmissionSemester.findById(
+      payload.admissionSemester,
+    );
+
+    userData.id = await generateStudentId(admissionSemester);
 
     const newUser = await User.create([userData], { session });
     // console.log(newUser);
@@ -44,21 +47,46 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
   }
 };
 
-const createFacultyIntoDB = async(password: string , payload: TFaculty)=>{
-  const session = await startSession()
+// =============================================================================
+
+const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
+  const session = await startSession();
   try {
-    session.startTransaction()
-  } catch (error) {
-    
+    session.startTransaction();
+
+    const userData: Partial<TUser> = {};
+    userData.password = password || (config.default_password as string);
+    userData.role = 'faculty';
+    const academicDepartment = await AcademicDepartment.findById(
+      payload.academicDepartment,
+    );
+    if (!academicDepartment) {
+      throw new Error('Academic department is not found');
+    }
+    userData.id = await generateFacultyId();
+
+    const newUser = await User.create([userData], { session });
+    if (!newUser.length) {
+      throw new Error('Failed to create user');
+    }
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id;
+    const newFaculty = await Faculty.create([payload], { session });
+
+    if (!newFaculty.length) {
+      throw new Error('Failed to create faculty');
+    }
+    await session.commitTransaction();
+    await session.endSession();
+    return newFaculty;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error('Failed to create faculty');
   }
 };
 
-
-
 export const userServices = {
   createStudentIntoDB,
-  createFacultyIntoDB
+  createFacultyIntoDB,
 };
-
-
-
--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -41,3 +41,39 @@ export const generateStudentId = async(payload: TAdmissionSemester) => {   // Au
   //                  '2030'                    '01'              '0001';
   return incrementId;
 };
+
+
+//============================================================================
+
+export const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne(
+    {
+      role: 'faculty',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyId();
+
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `${incrementId}`;
+
+  return incrementId;
+};
