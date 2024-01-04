82280afdcb3a7150ed1bd9efa6306a66650751a5
JobaerAbdi <jobaerrahman757@gmail.com>
2024-01-04 11:52:16 +0600

    update faulty validation schema created

--- src/modules/faculty/faculty.validation.ts
+++ src/modules/faculty/faculty.validation.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
+//            create faculty academic validation schema
 
-const facultyNameValidationSchema = z.object({
+const createFacultyNameValidationSchema = z.object({
     firstName: z.string({ required_error: 'FirstName is required' }),
     middleName: z.string({ required_error: 'MiddleName is required' }),
     lastName: z.string({ required_error: 'LastName is required' })
@@ -11,7 +12,7 @@ const createFacultyValidationSchema = z.object({
     facultyData: z.object({
       id: z.string({ required_error: 'ID is required' }),
       user: z.string({ required_error: 'User ID is required' }),
-      name: facultyNameValidationSchema,
+      name: createFacultyNameValidationSchema,
       gender: z.enum(['male', 'female'], {
         required_error: 'Gender is required',
       }),
@@ -41,6 +42,52 @@ const createFacultyValidationSchema = z.object({
   }),
 });
 
+//======================================================================================
+
+//              update faculty validation schema
+
+const updateFacultyNameValidationSchema = z.object({
+    firstName: z.string({ required_error: 'FirstName is required' }).optional(),
+    middleName: z.string({ required_error: 'MiddleName is required' }).optional(),
+    lastName: z.string({ required_error: 'LastName is required' }).optional()
+});
+
+const updateFacultyValidationSchema = z.object({
+  body: z.object({
+    facultyData: z.object({
+      id: z.string({ required_error: 'ID is required' }).optional(),
+      user: z.string({ required_error: 'User ID is required' }).optional(),
+      name: updateFacultyNameValidationSchema.optional(),
+      gender: z.enum(['male', 'female'], {
+        required_error: 'Gender is required',
+      }).optional(),
+      dateOfBirth: z.string({ required_error: 'Date of birth is required' }).optional(),
+      email: z.string({ required_error: 'Email is required' }).optional(),
+      contactNo: z.string({ required_error: 'Contact number is required' }).optional(),
+      emergencyContactNo: z.string({
+        required_error: 'Emergency contact number is required',
+      }).optional(),
+      presentAddress: z.string({
+        required_error: 'Present address is required',
+      }).optional(),
+      permanentAddress: z.string({
+        required_error: 'Permanent address is required',
+      }).optional(),
+      academicDepartment: z.string({
+        required_error: 'Academic department ID is required',
+      }).optional(),
+      academicFaculty: z.string({
+        required_error: 'Academic faculty ID is required',
+      }).optional(),
+      designation: z.string({ required_error: 'Designation is required' }),
+      profileImage: z.string({
+        required_error: 'Profile image URL is required',
+      }).optional(),
+    }),
+  }),
+});
+
 export const facultyValidation = {
   createFacultyValidationSchema,
+  updateFacultyValidationSchema
 };
