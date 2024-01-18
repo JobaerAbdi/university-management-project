aa5f4f27a1944779641875894dd01c2d92c914c6
JobaerAbdi <jobaerrahman757@gmail.com>
2024-01-18 07:22:13 +0600

    offered course zod validation is completed

---
+++ src/modules/offeredCourse/offeredCourse.validation.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { days } from "./offeredCourse.constant";
+
+export const createOfferedCourseSchemaValidation = z.object({
+    body: z.object({
+        offeredCourse: z.object({
+            semesterRegistration : z.string(),
+            admissionSemester: z.string(),
+            academicFaculty: z.string(), 
+            academicDepartment: z.string(), 
+            course: z.string(), 
+            faculty: z.string(),
+            maxCapacity: z.number(),
+            section: z.number(),
+            days: z.enum([...days] as [string, ...string[]]),
+            startTime: z.string(),
+            endTime: z.string()
+        })
+    })
+});
+
+export const updateOfferedCourseSchemaValidation = z.object({
+    body: z.object({
+        offeredCourse: z.object({
+            semesterRegistration : z.string().optional(),
+            admissionSemester: z.string().optional(),
+            academicFaculty: z.string().optional(), 
+            academicDepartment: z.string().optional(), 
+            course: z.string().optional(), 
+            faculty: z.string().optional(),
+            maxCapacity: z.number().optional(),
+            section: z.number().optional(),
+            days: z.enum([...days] as [string, ...string[]]).optional(),
+            startTime: z.string().optional(),
+            endTime: z.string().optional()
+        })
+    })
+});
+
+
+export const offeredCourseValidations = {
+    createOfferedCourseSchemaValidation,
+    updateOfferedCourseSchemaValidation
+};
