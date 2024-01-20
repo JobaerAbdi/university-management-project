6bd3d69fd668ed79974a5680cbabc0282853dfaf
JobaerAbdi <jobaerrahman757@gmail.com>
2024-01-20 13:54:41 +0600

    check all id by each model

--- src/modules/offeredCourse/offeredCourse.service.ts
+++ src/modules/offeredCourse/offeredCourse.service.ts
@@ -1,8 +1,38 @@
+import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
+import { AcademicFaculty } from "../academicFaculty/academicFaculty.model";
+import { Course } from "../course/course.model";
+import { Faculty } from "../faculty/faculty.model";
+import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./offeredCourse.interface";
 import { OfferedCourse } from "./offeredCourse.model";
 
 const createOfferedCourseIntoDB = async(payload: TOfferedCourse)=>{
     const {semesterRegistration,  academicFaculty, academicDepartment, course, faculty} = payload
+
+    const isSemesterRegistrationExists = await SemesterRegistration.findById(semesterRegistration)
+    if(!isSemesterRegistrationExists){
+        throw new Error("Semester registration is not found!")
+    }
+    
+    const isAcademicFacultyExists = await AcademicFaculty.findById(academicFaculty)
+    if(!isAcademicFacultyExists){
+        throw new Error ("Academic faculty is not found!")
+    }
+
+    const isAcademicDepartmentExists = await AcademicDepartment.findById(academicDepartment)
+    if(!isAcademicDepartmentExists){
+       throw new Error('Academic department is not found!')
+    }
+
+    const isCourseExists = await Course.findById(course)
+    if(!isCourseExists){
+        throw new Error('Course is not found!')
+    }
+
+    const isFacultyExists = await Faculty.findById(faculty)
+    if(!isFacultyExists){
+        throw new Error('Faculty is not found!')
+    }
     const result = await OfferedCourse.create(payload)
     return result
 };
